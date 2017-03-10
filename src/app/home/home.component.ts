2e5e529589cdd35381cd668f3db5ae301416106d
Lody Borgers <hi@lodybo.nl>
2017-03-10 23:28:16 +0100

    Added component boilerplate code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,28 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ContactService } from '../services/';
 
 @Component({
   selector: 'my-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  data = {
-    name: '',
-    email: '',
-    message: ''
-  };
+export class ContactComponent implements OnInit {
+    @ViewChild('contactForm') contactForm;
+    data = {
+        name: '',
+        email: '',
+        message: '',
+        url: ''
+    };
 
-  state = {
-      submitting: false,
-      submitted: false,
-      resend: false,
-      error: false
-  };
+    state = {
+        submitting: false,
+        submitted: false,
+        resend: false,
+        error: false
+    };
 
-  constructor() {
-    // Do stuff
-  }
+    constructor(private contactService: ContactService) { }
 
-  ngOnInit() { }
+    ngOnInit() { }
 
+    onSubmit() {
+        this.state.submitted = true;
+    }
+
+    resetForm() {
+        this.data = {
+            name: '',
+            email: '',
+            message: '',
+            url: ''
+        };
+
+        this.contactForm.reset();
+    }
+
+    sendForm() {
+        if (this.data.url === null) {
+            this.data.url = '';
+        }
+
+        // Set form state to submitting
+        this.state.submitting = true;
+
+        // Make REST call
+        this.contactService.sendMail(this.data)
+            .subscribe((res) => {
+                // Mark form as submitted
+                this.state.submitted = true;
+                this.state.submitting = false;
+            }, (err) => {
+                console.error('Error while sending: ', err);
+                this.state.submitting = false;
+                this.state.submitted = false;
+
+                this.state.error = true;
+            }, () => {
+                // Reset form, if no error was found
+                if (!this.state.error) {
+                    this.resetForm();
+                }
+            });
+    }
+
+    resendForm() {
+        this.state.submitted = false;
+        this.state.resend = true;
+    }
 }
