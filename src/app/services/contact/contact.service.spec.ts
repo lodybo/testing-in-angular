e8a29a4c2ba91e8cceda81fdd9774b8525ab6cef
Lody Borgers <hi@lodybo.nl>
2017-03-15 14:46:23 +0100

    Updated skeleton test code for ContactService

--- src/app/services/contact/contact.service.spec.ts
+++ src/app/services/contact/contact.service.spec.ts
@@ -3,3 +3,7 @@ import { MockBackend, MockConnection } from '@angular/http/testing';
 import { fakeAsync, inject, TestBed, tick } from '@angular/core/testing';
 import { BaseRequestOptions, Http, Response, ResponseOptions, XHRBackend } from '@angular/http';
 import { ContactService } from './contact.service';
+
+describe('Contact form', () => {
+
+});
